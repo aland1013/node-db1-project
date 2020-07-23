2a24a0c0d6733fbf8f96d30f8edd0d977756abac
aland1013 <adam.land@gmail.com>
2020-07-22 21:17:32 -0400

    finished POST and PUT endpoints

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -20,4 +20,30 @@ router.get('/:id', (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.post('/', (req, res) => {
+  const accountData = req.body;
+  // INSERT INTO accounts (...fields) VALUES (...values)
+  db('accounts')
+    .insert(accountData)
+    .then((id) => res.status(201).json({ data: id[0] }))
+    .catch((err) => console.log(err));
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  //UPDATE accounts SET field = 'new value' WHERE id = id;
+  db('accounts')
+    .where({ id })
+    .update(changes)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ data: count });
+      } else {
+        res.status(404).json({ message: 'there was no account to update' });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
